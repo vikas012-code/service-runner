7ca849f733b507a83afa6e832f85ecb623891fcb
vikas012-code <vikasthapa115@gmail.com>
2026-06-27 16:56:04 +0530

    refactor: update log messages in fun function for clarity

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const cron = require("node-cron");
 const app = express();
 
 async function fun () {
-    console.log("running a task every 1 minute");
+    console.log("running a fun every 1 minute");
 
     try {
         const res1 = await fetch("https://supamart-v-backend.onrender.com/")
         const res2 = await fetch("https://r-r-ornaments-backend.onrender.com")
         const res3 = await fetch("https://ai-project-backend-1vla.onrender.com/wake")
-        console.log("All APIs are running successfully");
+        console.log("All APIs are running successfully in fun");
     } catch (error) {
-        console.log("error ",{error});
+        console.log("error fun",{error});
     }
     
 }
