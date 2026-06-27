408f8980ee5fa359dbcbef28f5fc65468ea48784
vikas012-code <vikasthapa115@gmail.com>
2026-06-27 16:08:52 +0530

    Simplify success log for API fetch
    Removed API response logging from success message.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const job = cron.schedule("* 1 * * * *",async function() {
         const res1 = await fetch("https://supamart-v-backend.onrender.com/")
         const res2 = await fetch("https://r-r-ornaments-backend.onrender.com")
         const res3 = await fetch("https://ai-project-backend-1vla.onrender.com/wake")
-        console.log("All APIs are running successfully",{res1,res2,res3});
+        console.log("All APIs are running successfully");
     } catch (error) {
         console.log("error ",{error});
     }
